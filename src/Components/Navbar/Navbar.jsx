1121cb64bb78b0a588cf46fe0b0dbd6cd05ae52a
Rahim00001 <rahimlpt2022@gmail.com>
2023-12-21 17:02:57 +0600

    navbar added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,57 @@
-
+import { CiLogin, CiLogout } from "react-icons/ci";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
+    const navlinks = <>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/about'>About Us</NavLink></li>
+        <li><NavLink to='/contact'>Contact</NavLink></li>
+        <li><NavLink to='/login'>Login</NavLink></li>
+    </>
     return (
         <div>
-            <h1>This is navbar</h1>
+            <div className="navbar max-w-screen-xl mx-auto">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {navlinks}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">TaskCrafter</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navlinks}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <div className="dropdown dropdown-bottom dropdown-end">
+                        <div tabIndex={0}>
+                            <div className="avatar">
+                                <div className="w-10 mr-4 rounded-full  ring ring-primary ring-offset-base-100 ring-offset-2">
+                                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                </div>
+                            </div>
+                        </div>
+                        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                            <li className="text-center text-lg mb-1 font-bold">Name</li>
+                            <li className="text-center text-lg mb-1">sample@gmail.com</li>
+                            <li>
+                                <button className="btn text-xl">
+                                    Logout
+                                    <CiLogin className="mt-1"></CiLogin>
+                                </button>
+                                <button className="btn text-xl">
+                                    Login
+                                    <CiLogout className="mt-1"></CiLogout>
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
