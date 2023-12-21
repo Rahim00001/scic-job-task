911a74ecea8610ba746daaee8debb294c8df3324
Rahim00001 <rahimlpt2022@gmail.com>
2023-12-21 23:29:08 +0600

    firebase setup done

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,10 @@
+
+const Login = () => {
+    return (
+        <div>
+            Please Login
+        </div>
+    );
+};
+
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import {
 import Main from './Components/Main/Main.jsx';
 import Home from './Components/Home/Home.jsx';
 import ErrorPage from './Components/Error/ErrorPage.jsx';
+import Login from './Components/Login/Login.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
       }
     ]
   },
