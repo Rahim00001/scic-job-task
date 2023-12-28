20bb1c636f020884c5d8f9e8f247901afee98ee6
Rahim00001 <rahimlpt2022@gmail.com>
2023-12-28 18:49:31 +0600

    task added to database

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,33 +1,47 @@
 import Lottie from "lottie-react";
 import { useForm } from "react-hook-form";
-import registerAnim from "../../../public/register.json"
+import taskAnim from "../../../public/taskAdd.json"
 import Swal from "sweetalert2";
 const Dashboard = () => {
     const {
         register,
         handleSubmit,
-        // reset,
+        reset,
         formState: { errors },
     } = useForm();
 
     const onSubmit = data => {
         console.log(data)
-        Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: "Task Added Successful",
-            showConfirmButton: false,
-            timer: 1500
-        });
+        fetch('http://localhost:5000/tasks', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Task Added Successful",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    reset()
+                }
+            })
     };
     return (
         <div>
             <h2 className="text-6xl text-center">This is dashboard</h2>
             <div className="hero min-h-screen bg-base-100">
-                <div className="hero-content flex-col lg:flex-row-reverse lg:gap-20">
-                    <div className="text-center border lg:w-[500px]">
-                        <h1 className="text-5xl font-bold mb-3 lg:mb-8">Add your new task!</h1>
-                        <Lottie animationData={registerAnim}></Lottie>
+                <div className="hero-content flex-col lg:flex-row justify-center items-center lg:gap-20">
+                    <div className="text-center lg:w-[700px] border">
+                        <h1 className="text-5xl font-bold border -mb-12 lg:-mb-16">Add new task!</h1>
+                        <Lottie animationData={taskAnim}></Lottie>
                     </div>
                     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <form onSubmit={handleSubmit(onSubmit)} className="card-body">
@@ -59,7 +73,7 @@ const Dashboard = () => {
                                 {errors.description && <span className="text-red-600">Task description is required</span>}
                             </div>
                             <div className="form-control mt-6">
-                                <input type="submit" value="Add Now" className="btn bg-violet-600 text-white hover:bg-violet-800" />
+                                <input type="submit" value="Add Now" className="btn bg-blue-500 text-white hover:bg-blue-600" />
                             </div>
                         </form>
                     </div>
